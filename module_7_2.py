8cb9f0a55c7ad87c2dde42788a704470fe3834d4
Stella0910 <aniretake0910@gmail.com>
2024-11-13 15:55:09 +0300

    Add files via upload

--- module_7_2.py
+++ module_7_2.py
@@ -3,19 +3,14 @@ from pprint import pprint
 
 def custom_write(file_name, strings):
     name = file_name
-    _list_number = list()
-    _list_string = list()
-    _tell = list()
-    for number in range(len(strings)):
-        _list_number.append(number+1)
+    string_positions = {}
+    counter_lines = 0
     file = open(name, 'a', encoding='utf-8')
     for string in strings:
-        _tell.append(file.tell())
+        counter_lines += 1
+        string_positions[(counter_lines, file.tell())] = string
         file.write(f'{string}\n')
-        _list_string.append(string)
-    file.close()
-    strings_positions = dict(zip(zip(_list_number, _tell), _list_string))
-    return strings_positions
+    return string_positions
 
 
 info = [
